8b708d84a240e8dc0e04e55f5dd95b984c417afe
Vikas Kumar <v.ray96610@gmail.com>
2020-03-25 20:29:04 +0530

    Initial Commit

---
+++ src/main/java/com/gradlic/retrofitdemo/RetrofitdemoApplication.java
@@ -0,0 +1,13 @@
+package com.gradlic.retrofitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetrofitdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetrofitdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gradlic/retrofitdemo/RetrofitdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gradlic.retrofitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetrofitdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
